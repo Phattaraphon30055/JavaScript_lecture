d3971dd6ec051e0a9f73b0d4b3f466c3d475707a
Phattaraphon30055 <6406021630055@fitm.kmutnb.ac.th>
2024-12-26 23:32:18 +0700

    save

--- Lecture3/ex1.js
+++ Lecture3/ex1.js
@@ -1,6 +1,6 @@
-let values1=[ 'Apple', 1  , false ];
-let values2=[ 'Apple', 2  , true ];
-let values3=[ 'Mars', 9  , 'Apple' ];
+let values1=[ 'Fries','Apple', 1  , false ];
+let values2=[ 'Fries','Apple', 2  , true ];
+let values3=[ 'Fries','Mars', 9  , 'Apple' ];
 
 const commonElements = values1.filter(value => values2.includes(value) && values3.includes(value));
 
